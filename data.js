69e2faca215cbdbc430cb657a4aec123587e01ba
Marcella <marcellatsangos@gmail.com>
2020-05-05 16:59:24 -0300

    Dynamic data

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = [{
+  id: "2Y0HXnYpn9E",
+  title: "Hospedagem no Gitub Pages",
+  duration: "35 min",
+  price: "Free"
+}, {
+  id: "8h1qkjXFaDE",
+  title: "Curso Angular 9",
+  duration: "3 min",
+  price: "Free"
+},{
+  id: "hdDHg1p3YVc",
+  title: "Python 3",
+  duration: "30 min",
+  price: "Free"
+}, {
+  id: "1szPVvS5ax8",
+  title: "Aprenda Electron JS e Vue JS",
+  duration: "1h e 27 min",
+  price: "Free"
+}, {
+  id: "cSa-SMVMGsE",
+  title: "VueJS em 1 hora!",
+  duration: "55 min",
+  price: "Free"
+}, {
+  id: "yiDq9wUiUjc",
+  title: "Calculadora de calorias",
+  duration: "34 min",
+  price: "Free"
+}]
--- server.js
+++ server.js
@@ -2,14 +2,32 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 server.set('view engine', 'njk')
 
-nunjucks.configure('views', { express: server })
+nunjucks.configure('views', { express: server, autoescape: false })
 
-server.get('/', (req, res) => res.render('about'))
-server.get('/portfolio', (req, res) => res.render('portfolio'))
+server.get('/', (req, res) => {
+  const about = {
+    avartar_url: 'https://cdn.pixabay.com/photo/2015/07/28/21/58/student-865073_960_720.jpg',
+    name: 'Aulas de Graça',
+    role: 'Instrutores de Programação no YouTube',
+    description: 'Diversos conteúdos de programação totalmente gratuitos.',
+    links: [{
+      name: 'Github', url: 'https://github.com'
+    }, {
+      name: 'Twitter', url: 'https://twitter.com'
+    }, {
+      name: 'LinkedIn', url: 'https://linkedin.com'
+    }]
+  }
+
+  return res.render('about', { about })
+})
+
+server.get('/portfolio', (req, res) => res.render('portfolio', { items: videos }))
 
 server.use((req, res) => res.status(404).render('not-found'))
 
