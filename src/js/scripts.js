d27e9439e58b9c7e060b4fc87bf14790cde70cf6
Marcella <marcellatsangos@gmail.com>
2020-05-05 13:51:41 -0300

    Video

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,17 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 const modalClose = document.querySelector('.modal-close')
+const iframe = document.querySelector('iframe')
 
 for (let card of cards) {
-  card.addEventListener('click', () => modalOverlay.classList.add('active'))
+  card.addEventListener('click', () => {
+    const videoId = card.getAttribute('id')
+    modalOverlay.classList.add('active')
+    iframe.src = `https://www.youtube.com.br/embed/${videoId}`
+  })
 }
 
-modalClose.addEventListener('click', () => modalOverlay.classList.remove('active'))
+modalClose.addEventListener('click', () => {
+  modalOverlay.classList.remove('active')
+  iframe.src = ''
+})
