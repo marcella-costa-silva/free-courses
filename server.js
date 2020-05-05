520c48c837a7badb11d28ebda0570b8e890f8b51
Marcella <marcellatsangos@gmail.com>
2020-05-05 16:15:50 -0300

    Not found page

--- server.js
+++ server.js
@@ -11,4 +11,6 @@ nunjucks.configure('views', { express: server })
 server.get('/', (req, res) => res.render('about'))
 server.get('/portfolio', (req, res) => res.render('portfolio'))
 
+server.use((req, res) => res.status(404).render('not-found'))
+
 server.listen(5000, () => console.log('O servidor está rodando'))
